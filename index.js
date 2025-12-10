c969b3c7c4272b9514a0028fc80da33784ac3125
Sanzida Urmi <sanzidaurmi2002@gmail.com>
2025-12-10 18:22:32 +0600

    added verifyJWT

--- index.js
+++ index.js
@@ -31,7 +31,20 @@ app.use(
 app.use(express.json());
 
 // jwt middlewares
-
+const verifyJWT = async (req, res, next) => {
+  const token = req?.headers?.authorization?.split(' ')[1]
+  console.log(token)
+  if (!token) return res.status(401).send({ message: 'Unauthorized Access!' })
+  try {
+    const decoded = await admin.auth().verifyIdToken(token)
+    req.tokenEmail = decoded.email
+    console.log(decoded)
+    next()
+  } catch (err) {
+    console.log(err)
+    return res.status(401).send({ message: 'Unauthorized Access!', err })
+  }
+}
 
 
 
