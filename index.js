40a5b6367c903f6b6a08aa95554ca34047763353
Sanzida Urmi <sanzidaurmi2002@gmail.com>
2025-12-10 18:07:23 +0600

    added paidmodal code

--- index.js
+++ index.js
@@ -117,6 +117,14 @@ async function run() {
 
 
 
+           app.get("/paidmodal/:id", async (req, res) => {
+      const id  = req.params.id;
+      const query = {loanID : id};
+     
+      const cursor = paymentCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });  
 
             
 
